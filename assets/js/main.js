96ff16769b758dd94049d952e3cef74232e8b665
SuperCoder <propscode254@gmail.com>
2023-09-20 11:11:38 +0200

    first step works

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+// Save variables
+
+// inputs
+
+const stringInput = document.querySelector('[data-js="string"]');
+const stringSpliter = document.querySelector('[data-js="string-spliter"]');
+const beforeInput = document.querySelector('[data-js="davor"]');
+const afterInput = document.querySelector('[data-js="dannach"]');
+
+// outputs
+const output = document.querySelector('[data-js="output"]');
+
+// Function
+
+function split() {
+  // Save Values
+  let text = stringInput.value;
+  console.log(text);
+  let spliter = stringSpliter.value;
+  console.log(spliter);
+  let before = beforeInput.checked;
+  console.log(before);
+  let after = afterInput.checked;
+  console.log(after);
+
+  // find spliter
+
+  let stringIndex = text.indexOf(spliter);
+  console.log(stringIndex);
+
+  //  split the word with slice if before is true (checked)
+
+  let splitOutput = before
+    ? text.slice(stringIndex)
+    : text.slice(stringIndex + 1);
+
+  console.log(splitOutput);
+
+  output.innerHTML = splitOutput;
+}
